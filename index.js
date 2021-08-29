e9cbf57356b28a0d61015e60057f55b95b652413
Jagrit <jagritbudhiraja2@gmail.com>
2021-08-29 18:05:37 +0530

    Set up the view engine

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const port=8000;
 const app=express();
 // use express router app.use is a middleware
 app.use('/',require('./routes'));
-
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err)
     console.log(`error in running express js:${err}`);
